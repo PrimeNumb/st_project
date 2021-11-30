e23cdb3a59dcdb296a93ddf793ddee2df7ca9931
gali8033 <gabriel.lindgren.8033@student.uu.se>
2021-11-30 21:37:21 +0100

    statement coverage 88

--- tests/find_all.py
+++ tests/find_all.py
@@ -1,54 +1,38 @@
-#def _find_all(self, name, attrs, string, limit, generator, **kwargs):
-#        "Iterates over a generator looking for things that match."
-#
-#        if string is None and 'text' in kwargs:
-#            string = kwargs.pop('text')
-#            warnings.warn(
-#                "The 'text' argument to find()-type methods is deprecated. Use 'string' instead.",
-#                DeprecationWarning
-#            )
-#
-#        if isinstance(name, SoupStrainer):
-#            strainer = name
-#        else:
-#            strainer = SoupStrainer(name, attrs, string, **kwargs)
-#
-#        if string is None and not limit and not attrs and not kwargs:
-#            if name is True or name is None:
-#                # Optimization to find all tags.
-#                result = (element for element in generator
-#                          if isinstance(element, Tag))
-#                return ResultSet(strainer, result)
-#            elif isinstance(name, str):
-#                # Optimization to find all tags with a given name.
-#                if name.count(':') == 1:
-#                    # This is a name with a prefix. If this is a namespace-aware document,
-#                    # we need to match the local name against tag.name. If not,
-#                    # we need to match the fully-qualified name against tag.name.
-#                    prefix, local_name = name.split(':', 1)
-#                else:
-#                    prefix = None
-#                    local_name = name
-#                result = (element for element in generator
-#                          if isinstance(element, Tag)
-#                          and (
-#                              element.name == name
-#                          ) or (
-#                              element.name == local_name
-#                              and (prefix is None or element.prefix == prefix)
-#                          )
-#                )
-#                return ResultSet(strainer, result)
-#        results = ResultSet(strainer)
-#        while True:
-#            try:
-#                i = next(generator)
-#            except StopIteration:
-#                break
-#            if i:
-#                found = strainer.search(i)
-#                if found:
-#                    results.append(found)
-#                    if limit and len(results) >= limit:
-#                        break
-#        return results
+def _find_all(self, name, attrs, string, limit, generator, **kwargs):
+       if string is None and 'text' in kwargs:
+           string = kwargs.pop('text')
+           warnings.warn("The 'text' argument to find()-type methods is deprecated. Use 'string' instead.",DeprecationWarning)
+       if isinstance(name, SoupStrainer):
+           strainer = name
+       else:
+           strainer = SoupStrainer(name, attrs, string, **kwargs)
+       if string is None and not limit and not attrs and not kwargs:
+           if name is True or name is None:
+               result = (element for element in generator
+                         if isinstance(element, Tag))
+               return ResultSet(strainer, result)
+           elif isinstance(name, str):
+               if name.count(':') == 1:
+                   prefix, local_name = name.split(':', 1)
+               else:
+                   prefix = None
+                   local_name = name
+               result = (element for element in generator
+                         if isinstance(element, Tag) and (element.name == name) or (element.name == local_name and (prefix is None or element.prefix == prefix)))
+               return ResultSet(strainer, result)
+       results = ResultSet(strainer)
+       while True:
+           try:
+               i = next(generator)
+           except StopIteration:
+               break
+           if i:
+               found = strainer.search(i)
+               if found:
+                   results.append(found)
+                   if limit and len(results) >= limit:
+                       break
+       return results
+
+
+       # 34 lines with 4 misses => 1-4/34 = 88.24% statement coverage
