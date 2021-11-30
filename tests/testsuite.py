c85e114e8d6bdf7c3584671946244d6ba9c41b37
Sebbe Fällman <sebastian.fallman@live.com>
2021-11-30 11:56:32 +0100

    Added blackbox tests for traversing the tree, with functions: .tag, replace string on tag, .children, .name, .parent, .parents, .next_element, .next_elements, .sibling, .siblings, .next_element, .previous_element and .next_elements

--- tests/testsuite.py
+++ tests/testsuite.py
@@ -17,14 +17,14 @@ class SampleTestCase(unittest.TestCase):
         with self.assertRaises(TypeError):
             s.split(2)
 
-class TreeNavigationTest(unittest.TestCase):
+class TreeNavigationBlackboxTest(unittest.TestCase):
     html_text = """<html><head><title>The test title</title></head>
         <body>
             <p>This is a paragraph.</p>
-                <p>This is a paragraph with hyperlinks
-                    <a href="https://www.wikipedia.org/">This is a hyperlink to wikipedia!</a>
-                    <a href="https://www.facebook.com/">This is a hyperlink to facebook!</a>
-                </p>
+            <p>This is a paragraph with hyperlinks
+                <a href="https://www.wikipedia.org/">This is a hyperlink to wikipedia!</a>
+                <a href="https://www.facebook.com/">This is a hyperlink to facebook!</a>
+            </p>
         </body>
     """
 
@@ -52,10 +52,24 @@ class TreeNavigationTest(unittest.TestCase):
         for child in soup.body.children:
             body.append(child)
         
+        self.assertEqual(body[0].string, '\n')
         self.assertEqual(body[1].string, 'This is a paragraph.')
         self.assertEqual(body[2].string, '\n')
         self.assertEqual(body[3].string, None) # If a tag contains more than one string element, then .string is defined to be None:
         self.assertEqual(body[4].string, '\n')
+
+    def test_body_children_tag_name(self):
+        soup = BeautifulSoup(self.html_text, 'html.parser')
+        body = []
+
+        for child in soup.body.children:
+            body.append(child)
+        
+        self.assertEqual(body[0].name, None)
+        self.assertEqual(body[1].name, 'p')
+        self.assertEqual(body[2].name, None)
+        self.assertEqual(body[3].name, 'p')
+        self.assertEqual(body[4].name, None)
 
     def test_strings_for_all_body_children(self):
         soup = BeautifulSoup(self.html_text, 'html.parser')
@@ -85,7 +99,49 @@ class TreeNavigationTest(unittest.TestCase):
         self.assertEqual(parents[1], 'body')
         self.assertEqual(parents[2], 'html')
 
+    def test_next_sibling(self):
+        soup = BeautifulSoup(self.html_text, 'html.parser')
+        a = soup.a
 
+        self.assertEqual(a.string, 'This is a hyperlink to wikipedia!')
+        self.assertEqual(a.next_sibling.string, '\n')
+        self.assertEqual(a.next_sibling.next_sibling.string, 'This is a hyperlink to facebook!')
+
+    
+    def test_next_siblings(self):
+        soup = BeautifulSoup(self.html_text, 'html.parser')
+        a = soup.a
+
+        siblings = []
+        for sibling in a.next_siblings:
+            siblings.append(repr(sibling))
+
+        
+        self.assertEqual(siblings[0], "'\\n'")
+        self.assertEqual(siblings[1], '<a href="https://www.facebook.com/">This is a hyperlink to facebook!</a>')
+        self.assertEqual(siblings[2], "'\\n'")
+
+    def test_next_and_previous_element(self):
+        soup = BeautifulSoup(self.html_text, 'html.parser')
+        a = soup.a
+
+        next = a.next_element
+        previous = next.previous_element
+
+        self.assertEqual(a, previous)
+
+    def test_next_elements(self):
+        soup = BeautifulSoup(self.html_text, 'html.parser')
+        a = soup.a
+
+        elements = []
+        for element in a.next_elements:
+            elements.append(repr(element))
+        self.assertEqual(elements[0], "'This is a hyperlink to wikipedia!'")
+        self.assertEqual(elements[1], "'\\n'")
+        self.assertEqual(elements[2], '<a href="https://www.facebook.com/">This is a hyperlink to facebook!</a>')
+        self.assertEqual(elements[3], "'This is a hyperlink to facebook!'")
+        self.assertEqual(elements[4], "'\\n'")
 
 if __name__ == '__main__':
     unittest.main()
